0eace44675e7eb7a420af8e746dcf84935f27062
Paulo Ricardo <paulopentest@gmail.com>
2023-04-18 20:21:44 -0300

    primeiro commit

---
+++ desafio_01.rb
@@ -0,0 +1,8 @@
+#Palavra de inspiração - Colossenses 3:23-24
+puts "Digite seu nome: "
+nome = gets.chomp
+puts "Digite seu sobrenome: "
+sobrenome = gets.chomp
+puts "Informe a sua idade: "
+idade = gets.chomp.to_i
+puts "#{nome} #{sobrenome}, #{idade} Anos"
